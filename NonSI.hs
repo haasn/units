6970fec9c0d06f5ba48c7f45645bbbbec602deda
nand <nand@lavabit.com>
2012-11-09 18:53:28 +0100

    Add some non-SI units
    Ignore-this: 55b62622e5c3df41c05bf57885e61bd8
    
    darcs-hash:20121109175328-cd4f6-fcebf941334dae4041fb80c5c09ed77356cbf9fc

---
+++ NonSI.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes, DataKinds, TypeOperators #-}
+module Units.NonSI where
+
+import Units
+import Units.SI
+import Units.TH
+
+-- Some widely used non-SI units
+
+type Minute    = [u|min |]
+type Hour      = [u|h   |]
+type Day       = [u|d   |]
+type Degree    = [u|deg |]
+type ArcMinute = [u|amin|]
+type ArcSecond = [u|asec|]
+
+makeUnits [ ''Minute, ''Hour, ''Day, ''Degree, ''ArcMinute, ''ArcSecond ]
+
+type Are     = Hecto * Meter^2
+type Hectare = Hecto*Are
+type Liter   = (Deci*Meter)^3
+type Litre   = Liter
+type Gram    = Kilogram/Kilo
+type Tonne   = Mega*Gram
+
+makeUnits [ ''Are, ''Hectare, ''Liter, ''Litre, ''Gram, ''Tonne ]
+
+-- Experimentally determined units
+
+type ElectronVolt = [u|eV|]
+type AtomicMass   = [u|u |]
+type Dalton       = AtomicMass
+type AstroUnit    = [u|ua|]
+type Charge       = [u|e |]
+
+makeUnits [ ''ElectronVolt, ''AtomicMass, ''Dalton, ''AstroUnit, ''Charge ]
+
+-- Common units not officially sanctioned
+
+type Bar        = Deca^5 * Pascal
+type Millibar   = Milli*Bar
+type Atmosphere = [u|atm|]
+
+makeUnits [ ''Bar, ''Millibar, ''Atmosphere ]
