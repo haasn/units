44e784690df4c0d5590ab6067e53211f37efccec
nand <nand@lavabit.com>
2012-11-09 16:58:43 +0100

    Add more SI units
    Ignore-this: 3d639c5a26308df23aa2f99109712fea
    
    darcs-hash:20121109155843-cd4f6-ab6b745bf73e98afa2584a8d55012b998a3f78ec

--- SI.hs
+++ SI.hs
@@ -30,6 +30,25 @@ type Siemens = One / Ohm
 type Weber   = Joule / Ampere
 type Tesla   = Weber / (Meter*Meter)
 type Henry   = Weber / Ampere
+type Gray    = Joule / Kilogram
 
 makeUnits [ ''Hertz, ''Newton, ''Pascal , ''Joule, ''Watt , ''Coulomb, ''Volt,
             ''Farad, ''Ohm   , ''Siemens, ''Weber, ''Tesla, ''Henry ]
+
+-- These are technically equivalent to some existing units but are kept
+-- separate because they have different semantics
+
+type Mole       = [u|mol|]
+type Radian     = [u|rad|]
+type Steradian  = [u|sr|]
+type Celsius    = [u|dC|]
+type Becquerell = [u|Bq|]
+type Sievert    = [u|Sv|]
+
+makeUnits [ ''Mole, ''Radian, ''Steradian, ''Celsius, ''Becquerell, ''Sievert ]
+
+type Lumen = Candela * Steradian
+type Lux   = Lumen / (Meter*Meter)
+type Katal = Mole / Second
+
+makeUnits [ ''Lumen, ''Lux, ''Katal ]
