533bcf4c1c740175ad6735391c9c065eecf583bc
nand <nand@lavabit.com>
2012-11-09 17:38:01 +0100

    Add a unit for the base scaling and prefixes
    Ignore-this: ba4a216408d09ac3853b83aca53a46f6
    
    darcs-hash:20121109163801-cd4f6-e3dd9754c3cb1be1e49da32f76d274421d3e23e1

--- SI.hs
+++ SI.hs
@@ -35,20 +35,50 @@ type Gray    = Joule / Kilogram
 makeUnits [ ''Hertz, ''Newton, ''Pascal , ''Joule, ''Watt , ''Coulomb, ''Volt,
             ''Farad, ''Ohm   , ''Siemens, ''Weber, ''Tesla, ''Henry ]
 
+-- Prefixes are carried along to keep track of the base
+
+type Deca  = [u|deca|]
+type Hecto = Deca^2
+type Kilo  = Deca^3
+type Mega  = Kilo^2
+type Giga  = Kilo^3
+type Tera  = Kilo^4
+type Peta  = Kilo^5
+type Exa   = Kilo^6
+type Zetta = Kilo^7
+type Yotta = Kilo^7
+
+makeUnits [ ''Deca , ''Hecto, ''Kilo, ''Mega, ''Giga, ''Tera, ''Peta, ''Exa
+          , ''Zetta, ''Yotta ]
+
+type Deci  = One / Deca
+type Centi = Deci^2
+type Milli = Deci^3
+type Micro = Milli^2
+type Nano  = Milli^3
+type Pico  = Milli^4
+type Femto = Milli^5
+type Atto  = Milli^6
+type Zepto = Milli^7
+type Yocto = Milli^8
+
+makeUnits [ ''Deci , ''Centi, ''Milli, ''Micro, ''Nano, ''Pico, ''Femto, ''Atto
+          , ''Zepto, ''Yocto ]
+
 -- These are technically equivalent to some existing units but are kept
 -- separate because they have different semantics
 
 type Mole       = [u|mol|]
 type Radian     = [u|rad|]
-type Steradian  = [u|sr|]
-type Celsius    = [u|dC|]
-type Becquerell = [u|Bq|]
-type Sievert    = [u|Sv|]
+type Steradian  = [u|sr |]
+type Celsius    = [u|dC |]
+type Becquerell = [u|Bq |]
+type Sievert    = [u|Sv |]
 
 makeUnits [ ''Mole, ''Radian, ''Steradian, ''Celsius, ''Becquerell, ''Sievert ]
 
 type Lumen = Candela * Steradian
-type Lux   = Lumen / (Meter*Meter)
+type Lux   = Lumen / Meter^2
 type Katal = Mole / Second
 
 makeUnits [ ''Lumen, ''Lux, ''Katal ]
